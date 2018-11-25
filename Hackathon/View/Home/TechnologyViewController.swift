42a8162b9d517474edec80691be31bf108845929
tomoki69386 <tomoki_69386@yahoo.co.jp>
2018-11-26 06:40:50 +0900

    Update

--- Hackathon/View/Home/TechnologyViewController.swift
+++ Hackathon/View/Home/TechnologyViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import XLPagerTabStrip
 
 class TechnologyViewController: UIViewController {
-
+    
     private let tableView = UITableView()
     
     override func viewDidLoad() {
