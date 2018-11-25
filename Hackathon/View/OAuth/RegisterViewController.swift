0d2b24d50973e2bf13d818ba8587d82f745692d4
matsukishu <shu.matsuki.0625@gmail.com>
2018-11-26 05:38:11 +0900

    Update

--- Hackathon/View/OAuth/RegisterViewController.swift
+++ Hackathon/View/OAuth/RegisterViewController.swift
@@ -34,17 +34,10 @@ class RegisterViewController: UIViewController {
     
     
     @IBAction func registerButton(_ sender: UIButton) {
+    
+    self.performSegue(withIdentifier: "toProfile", sender: nil)
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- Hackathon/View/OficeViewController.swift
+++ Hackathon/View/OficeViewController.swift
@@ -10,21 +10,33 @@ import UIKit
 
 class OficeViewController: UIViewController {
 
+    
+    @IBOutlet weak var OficeImageView: UIImageView!
+    @IBOutlet weak var oficeNameLabel: UILabel!
+    @IBOutlet weak var headLineTextView: UITextView!
+    
+    @IBOutlet weak var placeLabel: UILabel!
+    @IBOutlet weak var personLabel: UILabel!
+    @IBOutlet weak var dayCountLabel: UILabel!
+    
+    @IBOutlet weak var presidentImageView: UIImageView!
+    @IBOutlet weak var presidentNameLabel: UILabel!
+    @IBOutlet weak var positionNameLabel: UITextView!
+    @IBOutlet weak var presidentIntroTextView: UITextView!
+    
+    @IBOutlet weak var headLine2Label: UILabel!
+    @IBOutlet weak var intro2TextView: UITextView!
+    
+    
+    @IBOutlet weak var headLine3Label: UILabel!
+    @IBOutlet weak var intro3TextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func playButtonPressed(_ sender: Any) {
     }
-    */
-
+    
 }
