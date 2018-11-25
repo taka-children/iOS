39b87cc5c0ccd2756471684afacb94ce919618a7
tomoki69386 <tomoki_69386@yahoo.co.jp>
2018-11-26 05:03:52 +0900

    Add internetVC

---
+++ Hackathon/View/Home/HomeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HomeTableViewCell.swift
+//  Hackathon
+//
+//  Created by 築山朋紀 on 2018/11/26.
+//  Copyright © 2018 tomoki. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
