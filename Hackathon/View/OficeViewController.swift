2014ce1cb7c93036116349c62d714a6640199112
matsukishu <shu.matsuki.0625@gmail.com>
2018-11-26 04:52:53 +0900

    Add OficeUI

---
+++ Hackathon/View/OficeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  OficeViewController.swift
+//  Hackathon
+//
+//  Created by 松木周 on 2018/11/26.
+//  Copyright © 2018年 tomoki. All rights reserved.
+//
+
+import UIKit
+
+class OficeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
