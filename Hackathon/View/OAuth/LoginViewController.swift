c8ea78db6dd0ecc8e1e5eb22fdf2ad0978cec4f0
tomoki69386 <tomoki_69386@yahoo.co.jp>
2018-11-25 22:45:16 +0900

    Fix LoginVC

--- Hackathon/View/OAuth/LoginViewController.swift
+++ Hackathon/View/OAuth/LoginViewController.swift
@@ -9,29 +9,29 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
-    @IBOutlet weak var label: UILabel!
-    @IBOutlet weak var userID: UITextField!
-    @IBOutlet weak var password: UITextField!
     
-    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet private weak var mailInput: UITextField!
+    @IBOutlet private weak var password: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setLayout()
+    }
+    
+    private func setLayout() {
+        mailInput.layer.cornerRadius = 25
+        mailInput.layer.borderWidth = 1
+        mailInput.layer.borderColor = AppColor.main.cgColor
+        mailInput.backgroundColor = AppColor.white
+        
+        password.layer.cornerRadius = 25
+        password.layer.borderWidth = 1
+        password.layer.borderColor = AppColor.main.cgColor
+        password.backgroundColor = AppColor.white
     }
     
     @IBAction func loginButton(_ sender: UIButton) {
+        
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
