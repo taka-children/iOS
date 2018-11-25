1f482a10b57d9aee665f2ee0688486ca427bf4d3
tomoki69386 <tomoki_69386@yahoo.co.jp>
2018-11-26 06:46:34 +0900

    Fix セグエ

--- Hackathon/View/OficeViewController.swift
+++ Hackathon/View/OficeViewController.swift
@@ -36,8 +36,5 @@ class OficeViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-    @IBAction func playButtonPressed(_ sender: Any) {
-        self.performSegue(withIdentifier: "toGoogleForm", sender: nil)
-    }
     
 }
