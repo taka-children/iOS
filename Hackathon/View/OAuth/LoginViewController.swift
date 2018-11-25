8b1105d975523e23127ca70eafed676427a7f878
Kazuki Kishishita <kishi-tennis0310@rakuten.jp>
2018-11-25 20:46:13 +0900

    Add OAuthUI

---
+++ Hackathon/View/OAuth/LoginViewController.swift
@@ -0,0 +1,37 @@
+//
+//  LoginViewController.swift
+//  Hackathon
+//
+//  Created by 岸下和樹 on 2018/11/25.
+//  Copyright © 2018 tomoki. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var userID: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func loginButton(_ sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
