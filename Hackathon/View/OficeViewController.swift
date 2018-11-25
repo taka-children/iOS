4648047ecc15941fdf7c6248d2f519517d531ac7
tomoki69386 <tomoki_69386@yahoo.co.jp>
2018-11-26 07:00:39 +0900

    Fix oficalImage

--- Hackathon/View/OficeViewController.swift
+++ Hackathon/View/OficeViewController.swift
@@ -7,11 +7,12 @@
 //
 
 import UIKit
+import SDWebImage
 
 class OficeViewController: UIViewController {
 
     
-    @IBOutlet weak var OficeImageView: UIImageView!
+    @IBOutlet weak var oficeImageView: UIImageView!
     @IBOutlet weak var oficeNameLabel: UILabel!
     @IBOutlet weak var headLineTextView: UITextView!
     
@@ -33,8 +34,15 @@ class OficeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        navigationItem.title = "株式会社taliki"
+        
+        let oficeURL = URL(string: "https://taliki.org/wp-content/uploads/2017/05/IMG_5945.jpg")
+        oficeImageView.sd_setImage(with: oficeURL)
+        let presidentURL = URL(string: "https://scontent-nrt1-1.xx.fbcdn.net/v/t1.0-9/30443186_804034783136035_4901413405550947549_n.jpg?_nc_cat=102&_nc_ht=scontent-nrt1-1.xx&oh=058c0be0cfeac6137c9a143440e884e2&oe=5CA79DC8")
+        presidentImageView.sd_setImage(with: presidentURL)
+        presidentImageView.layer.cornerRadius = presidentImageView.frame.width / 2
+        presidentImageView.clipsToBounds = true
     }
     
 }
