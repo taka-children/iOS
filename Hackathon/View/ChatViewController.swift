e1b35d9532cdeeb19d6b93d727610d386649ab4d
tomoki69386 <tomoki_69386@yahoo.co.jp>
2018-11-26 04:15:06 +0900

    Add chat表示

--- Hackathon/View/ChatViewController.swift
+++ Hackathon/View/ChatViewController.swift
@@ -7,17 +7,29 @@
 //
 
 import UIKit
+import SDWebImage
 
 class ChatViewController: UIViewController {
   
-    @IBOutlet private weak var chatTableView: UITableView!
+    private let tableView = UITableView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         navigationItem.title = "チャット"
-        chatTableView.delegate = self
-        chatTableView.dataSource = self
+        let nib = UINib(nibName: "ChatTableViewCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "chatCell")
+        self.view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        tableView.snp.makeConstraints { make in
+            make.size.equalTo(self.view)
+        }
     }
 }
 
@@ -27,7 +39,14 @@ extension ChatViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "chatCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "chatCell") as! ChatTableViewCell
+        
+        cell.userImageView.layer.cornerRadius = 20
+        cell.userImageView.clipsToBounds = true
+        let imageURL = URL(string: "https://pbs.twimg.com/profile_images/1061520538386915329/ExNUPGbF_400x400.jpg")
+        cell.userImageView.sd_setImage(with: imageURL)
+        cell.nameLabel.text = "ともき"
+        cell.messageLabel.text = "ほげほげほげおｈごえ"
         return cell
     }
 }
