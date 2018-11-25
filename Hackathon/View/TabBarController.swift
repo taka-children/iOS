428d6a52eaa0de6a7086f46b012ff560e02ad160
tomoki69386 <tomoki_69386@yahoo.co.jp>
2018-11-25 22:20:01 +0900

    Fix

---
+++ Hackathon/View/TabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarController.swift
+//  Hackathon
+//
+//  Created by 築山朋紀 on 2018/11/25.
+//  Copyright © 2018 tomoki. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
